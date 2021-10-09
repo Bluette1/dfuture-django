340df63da5548d4527a986a61d4a3fa6ca912c71
Bluette1 <marylene.sawyer@gmail.com>
2021-10-09 07:34:04 +0200

    Add models and serializer

--- dfuture/models.py
+++ dfuture/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+def upload_to(instance, filename):
+  return 'uploads/filename'.format(filename=filename)
 
 class DocumentRequest(models.Model):
-    name = models.CharField(max_length=20)
-    uploaded = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+  name = models.CharField(max_length=20)
+  uploaded = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+    ordering = ['-created_at',]
+  
+  def __str__(self):
+    return self.name
     
-class Meta:
-  order = ['-created_at',]
+class Document(models.Model):
+  name = models.CharField(max_length=20)
+  client_name = models.CharField(max_length=20)
+  uploaded_at = models.DateTimeField(auto_now_add=True)
+  file = models.FileField(_("File"), upload_to=upload_to)
+    
+  def __str__(self):
+      return self.name
--- dfuture/views.py
+++ dfuture/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from .serializers import DocumentSerializer
+from .models import Document
+from .serializers import DocumentRequestSerializer
+from .models import DocumentRequest
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+class DocumentView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get(self, request, *args, **kwargs):
+        documents = Document.objects.all()
+        serializer = DocumentSerializer(documents, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        documents_serializer = DocumentSerializer(data=request.data)
+        if documents_serializer.is_valid():
+            documents_serializer.save()
+            return Response(documents_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', documents_serializer.errors)
+            return Response(documents_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
